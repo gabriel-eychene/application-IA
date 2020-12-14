6578eb2f5aa2bcb10164da546191b3e117d2db0e
gabriel-eychene <sacremme@gmail.com>
2020-12-14 19:58:10 +0100

    Solution fonctionne mais contraintes fausse

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.Solution;
 
 public class Main 
 {
 	public static void main(final String[] args)
 	{
-		Model model = new Model("test model");
-		System.out.println(model.toString());
+		Model model = new Model("Sel et Moutarde");
+
+		BoolVar bs = model.boolVar("Bs");
+		BoolVar bm = model.boolVar("Bm");
+		BoolVar cs = model.boolVar("Cs");
+		BoolVar cm = model.boolVar("Cm");
+		BoolVar ds = model.boolVar("Ds");
+		BoolVar dm = model.boolVar("Dm");
+		BoolVar ls = model.boolVar("Ls");
+		BoolVar lm = model.boolVar("Lm");
+		BoolVar ms = model.boolVar("Ms");
+		BoolVar mm = model.boolVar("Mm");
+
+		model.allEqual(bs, bm, cs, cm, ds, dm, ls, lm, ms, mm).post();
+		Solution solution = model.getSolver().findSolution();
+		if(solution != null)
+		{
+			System.out.println(solution.toString());
+		}
+
 	}
 }
